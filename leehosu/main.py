2844449b381ad27cdbb71952eb99f5cd667e9e95
leehosu <hosu4549@gmail.com>
2024-07-24 16:58:26 +0900

    [update] redirect 설정
    Signed-off-by: leehosu <hosu4549@gmail.com>

--- leehosu/main.py
+++ leehosu/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return RedirectResponse(url="/api/v1/leehosu")
 
 @app.get("/api/v1/leehosu")
 def root():
