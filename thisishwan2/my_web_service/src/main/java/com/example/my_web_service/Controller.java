1448f6169885ac0d887ae29c700700db90c197db
thisishwan2 <feel000617@gmail.com>
2024-07-24 22:32:20 +0900

    feat: / 경로 api 추가
    Signed-off-by: thisishwan2 <feel000617@gmail.com>

--- thisishwan2/my_web_service/src/main/java/com/example/my_web_service/Controller.java
+++ thisishwan2/my_web_service/src/main/java/com/example/my_web_service/Controller.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, World!";
+    }
+
     @GetMapping("/healthcheck")
     public String healthcheck() {
         return "OK";
